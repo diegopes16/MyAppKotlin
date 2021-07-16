a282941cbf14901e6db3c2ba5c4ffb89900c181e
diegopes16 <diego.cunha@ufba.br>
2021-07-15 22:00:08 -0300

    Semana 1 Aula 04

--- app/src/main/java/br/com/localiza/objeto.kt
+++ app/src/main/java/br/com/localiza/objeto.kt
@@ -6,3 +6,10 @@ abstract class  Animal {
 
 class  Gato:Animal(){}
 class  Cachorro:Animal(){}
+
+class MaeDePet {
+    var meusPets:Array<Animal?> = Array(5){null}
+}
+
+//when
+//idade in 0..17 -> print(nome)
