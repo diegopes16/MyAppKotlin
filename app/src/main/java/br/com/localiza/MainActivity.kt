bdb68ec06e111dcb509ff5767f3b9ce55651eca0
diegopes16 <diego.cunha@ufba.br>
2021-07-16 22:02:37 -0300

    Primeira Semana Aula 05

--- app/src/main/java/br/com/localiza/MainActivity.kt
+++ app/src/main/java/br/com/localiza/MainActivity.kt
@@ -7,5 +7,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(ehUmBomFilme("Vingadores: Endgame"))
+        criarNumeros()
     }
-}
+
+    fun ehUmBomFilme(filme: String?):String{
+        return when (filme?.length){
+            null,0 ->  "Erro, preciso de um nome pra avaliar"
+            in 1..4 ->  "Um nome tão curto não pode ser bom"
+            else ->  "É, talvez seja bom"
+        }
+    }
+
+    fun criarNumeros(){
+        val lista = List(10){it+1}
+        var listamutavel = mutableListOf<String>()
+        lista.forEach(){
+            listamutavel.add(if(it%2==0) it.toString() else "-")
+        }
+        println(listamutavel)
+    }
+ }
--- app/src/main/java/br/com/localiza/objeto.kt
+++ app/src/main/java/br/com/localiza/objeto.kt
@@ -13,3 +13,9 @@ class MaeDePet {
 
 //when
 //idade in 0..17 -> print(nome)
+// for (item in collection) print(item)
+// for (item: Int in ints){}
+// for (i in 1..10){}
+// for (i in 12 downTo 6){}
+//eu.meusPets.forEach{} Pra cada item ele faz uma ação
+//while (x>0){}
